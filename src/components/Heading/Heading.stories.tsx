229e9717d467cec43e8fda24b589adc131316470
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-10-29 07:05:04 +0530

    fix: remove <Provider />

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Heading } from './Heading';
-import { Provider } from '..';
 
 storiesOf('Heading', module)
-  .add('Default', () => (
-    <Provider>
-      <Heading>Hello!</Heading>
-    </Provider>
-  ))
+  .add('Default', () => <Heading>Hello!</Heading>)
   .add('Color', () => <Heading color='red'>This should be red</Heading>);
--- src/components/Provider/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '@fontsource/inter';
-import '@fontsource/inter/800.css';
-
-export interface ProviderProps {}
-
-export const Provider: React.FC<ProviderProps> = ({ children }) => {
-  return <>{children}</>;
-};
